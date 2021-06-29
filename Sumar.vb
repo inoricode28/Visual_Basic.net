025a5dfe6f930dc95100511efc36afa6ff0c7001
inoricode28 <inori237@gmail.com>
2021-06-29 17:45:15 -0500

    VB01

---
+++ Sumar.vb
@@ -0,0 +1,16 @@
+Module Sumar
+Sub Main()
+Dim a, b, c As Integer
+While True
+System.Console.Write("Ingrese su primer numero: ")
+a = System.console.Readline()
+System.Console.Write("Ingrese su segundo numero: ")
+b = System.console.Readline()
+'Calcular
+C = A + B
+'Mostrar en Pantalla
+System.Console.Writeline("El resultado es: " & C)
+System.console.Readline()
+End While
+End Sub 
+End Module
