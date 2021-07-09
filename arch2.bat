e81af929f7cda0df20546e6030682754cfae2af4
inoricode28 <inori237@gmail.com>
2021-07-09 18:59:43 -0500

    V08

---
+++ arch2.bat
@@ -0,0 +1,14 @@
+@echo off
+title Creador de archivo
+@echo off
+rem Programa: Scanear.BAT
+echo ********************************************
+echo ** Este programa Fuciona el Mp3 con una   **
+echo ** Imagen cualquiera, cabe destacar       **
+echo ********************************************
+set/p nom=Nombre del archivo: 
+echo Archivo creado %nom%!!
+type nul > %nom%
+pause>nul
+
+
